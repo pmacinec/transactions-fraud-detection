856b511ab72f46add5fb8a5bda78557a4eeb7621
Timotej Zaťko <timi.zatko@gmail.com>
2020-03-21 13:07:53 +0100

    Remove useless files downloaded from kaggle and rename them

--- download_data.sh
+++ download_data.sh
@@ -1,4 +1,7 @@
-rm -rf ieee-fraud-detection.zip
 kaggle competitions download -c ieee-fraud-detection
 mkdir -p data
 unzip -o ieee-fraud-detection.zip -d data
+rm -rf ieee-fraud-detection.zip
+rm ./data/sample_submission.csv ./data/test_identity.csv ./data/test_transaction.csv
+mv ./data/train_identity.csv ./data/identities.csv
+mv ./data/train_transaction.csv ./data/transactions.csv
