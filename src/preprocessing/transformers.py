1aae406fd66b5449bd19d9bb41a2bf4d6f72d300
Peter Mačinec <peter.macinec@gmail.com>
2020-04-04 15:50:53 +0200

    Add Normalizer transformer

--- src/preprocessing/transformers.py
+++ src/preprocessing/transformers.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import normalize
 
 
 class SelectFeatures(BaseEstimator, TransformerMixin):
@@ -60,12 +61,57 @@ class EmailProviderTransform(TransformerMixin, BaseEstimator):
         return self
 
     def transform(self, x, y=None, copy=None):
-        def transform_email(val):
-            if val is np.nan:
-                return val
-            return val.split('.')[0]
+        def transform_email(value):
+            """
+            Transform email domains into domain names only.
 
-        for column in self.columns:
-            x[column] = x[column].astype('str').apply(transform_email).astype('str')
+            We have found in the data, that some email domains are
+            repeated (e.g. `gmail` and `gmail.com`). Those domains
+            should be aggregated into simply `gmail`.
+
+            :param value: value to be transformed.
+            :return: transformed email domain.
+            """
+            if value is np.nan:
+                return value
+            return value.split('.')[0]
+
+        for col in self.columns:
+            x[col] = x[col].astype('str').apply(transform_email).astype('str')
 
         return x
+
+
+class Normalizer(TransformerMixin):
+    """Normalize numerical attributes of dataframe."""
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, df):
+        df = (df - df.mean()) / df.std()
+        return df
+
+
+class OutliersFilter(TransformerMixin):
+    """Remove outliers using quartiles."""
+
+    def __init__(self):
+        self.Q1 = {}
+        self.Q3 = {}
+
+    def fit(self, df, y=None, **fit_params):
+        for col in df.columns:
+            self.Q1[col] = df[col].quantile(.25)
+            self.Q3[col] = df[col].quantile(.75)
+        return self
+
+    def transform(self, df, **transform_params):
+        for col in df.columns:
+            Q1 = self.Q1[col]
+            Q3 = self.Q3[col]
+            lower_outlier = Q1 - (Q3 - Q1) * 1.5
+            upper_outlier = Q3 + (Q3 - Q1) * 1.5
+
+            df = df[(df[col] >= lower_outlier) & (df[col] <= upper_outlier)]
+        return df
